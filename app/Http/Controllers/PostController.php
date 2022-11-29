5b947c1857802d42d0c2187826839a0051e030a7
Md Aqueeb <mdaqueeb@gmail.com>
2022-11-29 18:21:01 +0530

    REST API without authentication

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return Post::all();
+    }
+    public function store(Request $request)
+    {
+        try {
+            $post = new Post();
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if ($post->save()) {
+                return response()->json(['status' => 'success', 'message'=> 'Post created cuccessfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message'=> $e->getMessage()]);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            $post->title = $request->title;
+            $post->body = $request->body;
+            if ($post->save()) {
+                return response()->json(['status' => 'success', 'message'=> 'Post updated cuccessfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message'=> $e->getMessage()]);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $post = Post::findOrFail($id);
+            if ($post->delete()) {
+                return response()->json(['status' => 'success', 'message'=> 'Post deleted cuccessfully']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message'=> $e->getMessage()]);
+        }
+    }
+}
